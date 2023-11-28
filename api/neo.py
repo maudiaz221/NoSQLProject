350cbe3dd80316b8433f0e6f030240c77a012998
Mauricio Diaz <diazmauricio221@gmail.com>
2023-11-27 19:36:00 -0600

    ya listo el neo4j

--- api/neo.py
+++ api/neo.py
@@ -16,17 +16,42 @@ def main():
             f"CREATE (n:{node_label}) SET n = row"
         )
         return query
+    
 
     # Load CSV files into Neo4j
-    csv_files =  ['https://raw.githubusercontent.com/maudiaz221/NoSQLProject/main/locations.csv','https://raw.githubusercontent.com/maudiaz221/NoSQLProject/main/episodes.csv','https://raw.githubusercontent.com/maudiaz221/NoSQLProject/main/characters.csv']
+    csv_files =  ['https://raw.githubusercontent.com/maudiaz221/NoSQLProject/main/data/locations.csv','https://raw.githubusercontent.com/maudiaz221/NoSQLProject/main/data/episodes.csv','https://raw.githubusercontent.com/maudiaz221/NoSQLProject/main/data/characters.csv']
 
     node_labels = ["location", "episode", "character"]
+    
+    query1 = '''
+        MATCH (c:character),(e:episode)
+        WHERE c.episode_id = e.episode_id
+        CREATE (c)-[r:APPEARED_IN]->(e)
+    '''
+    query2 = '''
+        MATCH (c:character),(l:location)
+        WHERE c.character_id = l.character_id
+        CREATE (c)-[r:LOCATED_IN]->(l)
+    '''
 
     with GraphDatabase.driver(uri, auth=(username, password)) as driver:
         for csv_file, node_label in zip(csv_files, node_labels):
             with driver.session() as session:
                 query = load_csv(csv_file, node_label)
                 session.run(query)
+                try:
+                    session.run(query1)
+                    session.run(query2)
+                except:
+                    print("Error al crear las relaciones")
+    
+    print("Datos insertados")
+                    
+                
+   
+    
+    
+    
                 
 if __name__ == "__main__":
     main()
